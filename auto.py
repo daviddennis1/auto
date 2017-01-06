729d38101dbe75866f8f55517fa7ef6c1567d7e7
David Dennis <david@flashpoint-intel.com>
2017-01-06 17:31:55 -0500

    Daddy

--- auto.py
+++ auto.py
@@ -4,25 +4,180 @@ from subprocess import Popen, PIPE
 from config import SETTINGS, TEXT
 
 
-def args_to_kwargs(args):
-    d = {}
-    for k,v in args._get_kwargs():
-        d[k] = v
-    return d
+class Util:
+    def add_in_list(s, new_item, num, llist, one_line=False):
+        if isinstance(new_item, str) and not one_line:
+            new_item = new_item.split('\n')
+        # print(llist[:num][-1], '!')
+        # print(llist[num:][0], '!')
+        llist = llist[:num] + [new_item] + llist[num:]
+        # print(llist[num-2:num+2])
+        return llist
 
-def run_shell_cmd(cmd, silent=False):
-    if not silent:
-        print('\tRunning: "%s"' % ' '.join(cmd))
-    s = Popen(cmd , stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    lines = [o for o in s.stdout]
-    if lines:
-        print('\tRun Command Output:')
-        for l in lines:
-            print('\t\t'+l.decode('utf-8').replace('\n', ''))
-    return lines
+    def log(s, text, tabs=0):
+        print('\t'*tabs + text)
 
 
-class Main:
+class OSUtil(Util):
+    def __init__(s, *args, **kwargs):
+        s.auto_dir = os.getcwd()
+
+    def get_full_path(s, path):
+        if path[0] != '/':
+            path = '/' + path
+        if not s.auto_dir in path:
+            new_path = s.auto_dir + path
+            return new_path
+        return path
+
+    def is_a_path(s, string):
+        if s.auto_dir in string:
+            return True
+        if '/' in string:
+            return True
+        return False
+
+    def run_shell_cmd(s, cmd, silent=False):
+        if not silent:
+            print('\tRunning: "%s"' % ' '.join(cmd))
+        #print(cmd, '!!')
+        output, error = Popen(cmd , stdout=PIPE, stderr=PIPE).communicate()
+        if error:
+            print('PSQL ERROR:')
+            for el in error.decode('utf-8').split('\n'):
+                if el:
+                    print('\t'+el)
+        lines = output.decode('utf-8').split('\n')
+        if lines and lines != ['']:
+            print('\tRun Command Output:')
+            for l in lines:
+                print('\t\t'+l.replace('\n', ''))
+        return lines
+
+    def mv(s, old_file_path, new_file_path, silent=False):
+        s.call('mv %s %s' % (old_file_path, new_file_path), silent=silent)
+
+    def rm(s, file_path, silent=False):
+        s.call('rm %s' % file_path, silent=silent)
+
+    def call(s, cmd, silent=False):
+        if isinstance(cmd, str):
+            cmd = [c for c in cmd.split(' ') if c != '']
+
+        #if not s.args.get('test_run'): # TODO: <--
+        s.run_shell_cmd(cmd, silent=silent)
+
+    def file_exists(s, file_path):
+        return os.path.exists(file_path)
+
+
+class DBUtil: # (OSUtil)
+    def run_sql(s, sql_cmd):
+        db_cmd = ['psql', '-c', sql_cmd]
+        s.run_shell_cmd(db_cmd)
+
+
+class File(OSUtil):
+    def __init__(s, *args, **kwargs):
+        super(File, s).__init__(*args, **kwargs)
+        file_path = args[0]
+        content = args[1] if len(args) > 1 else None
+
+        s.file_path = s.get_full_path(file_path)
+
+        s.load_file_contents(content)
+
+    def load_file_contents(s, content=None):
+        if content:
+            s.content = content
+        else:
+            with open(s.file_path) as f:
+                s.content = f.read()
+        s.lines = s.content.split('\n')
+
+    def insert_in_file(s, pattern, new_line):
+        line_number = s.find_line_number(pattern)
+        new_lines = s.add_in_list(new_line, line_number, s.lines, one_line=True)
+        s.write_to_file(new_lines)
+
+    def write_to_file(s, content, file_path=None):
+        if file_path:
+            file_path = s.get_full_path(file_path)
+        else:
+            file_path = s.file_path
+
+        line_mode = False
+        if isinstance(content, list) or isinstance(content, tuple):
+            line_mode = True
+
+        tmp_file_path = s.archive_file(file_path)
+
+        with open(s.file_path, 'w') as f:
+            if line_mode:
+                for line in content:
+                    if isinstance(line, list):
+                        for l in line:
+                            f.write(l+'\n')
+                        continue
+                    f.write(line+'\n')
+            else:
+                f.write(content)
+
+        s.load_file_contents()
+
+        s.rm(tmp_file_path, silent=True)
+
+    def remove_lines(s, beg_pattern, end_pattern=None):
+        beg_line_num = s.find_line_number(beg_pattern)
+        if end_pattern:
+            end_line_num = s.find_line_number(end_pattern, after=beg_line_num)
+        else:
+            end_line_num = None
+        #print(beg_pattern,  beg_line_num)
+        s.remove_line_nums(beg_line_num, end_line_num)
+
+    def remove_line_nums(s, beg_num, end_num=None):
+        # print(beg_num, end_num, '>>')
+        # print(s.lines[:100])
+        # print(1)
+        if end_num:
+            new_lines = s.lines[:beg_num-1] + s.lines[end_num:]
+        else:
+            new_lines = s.lines[:beg_num-1] + s.lines[beg_num:]
+        # print(new_lines[:100])
+        # print(2)
+        s.write_to_file(new_lines)
+
+    def find_line_number(s, pattern, after=0):
+        line_area = s.lines[after:]
+        # print(pattern, after)
+        for i, line in enumerate(line_area, start=1):
+            if pattern in line:
+                return i + after
+        return None
+
+    def archive_file(s, file_path):
+        tmp_file_path = file_path + '.old'
+        if s.file_exists(file_path):
+            s.mv(file_path, tmp_file_path, silent=True)
+        return tmp_file_path
+
+    def file_exists(s, file_path):
+        if os.path.exists(file_path):
+            return True
+        return False
+
+    # def get_file_obj(s, file_pointer):
+    #     file_obj = None
+    #     if s.is_a_path(file_pointer):
+    #         file_path = file_pointer
+    #         file_obj = File(file_path)
+    #     elif isinstance(file_pointer, File):
+    #         file_obj = file_pointer
+    #     return file_obj
+
+
+class Main(OSUtil, DBUtil):
 
     def __init__(s, *args, **kwargs):
         # print(kwargs)
@@ -30,11 +185,10 @@ class Main:
         for k,v in kwargs.items():
             s.args[k] = v
 
+        super(Main, s).__init__(*args, **kwargs)
         s.setup()
-        s.create_django_project()
 
     def setup(s):
-        s.auto_dir = os.getcwd()
         s.reset()
 
     def reset(s):
@@ -57,14 +211,20 @@ class Main:
         s.cd(proj_name)
         s.startapp()
         s.add_app_to_settings()
-        s.migrate()
+        s.add_db_to_settings()
         s.create_mng_cmd_dir()
         s.create_demo_mng_cmd()
         s.log('Created Management Command Directory + Stub command')
         s.create_models()
+        s.migrate()
+        s.add_models_to_admin()
 
     def migrate(s):
+        create_db_cmd = 'CREATE DATABASE %s;' % s.proj_name
+        s.run_sql(create_db_cmd)
+
         s.cd(s.proj_dir)
+        s.call('python manage.py makemigrations')
         s.call('python manage.py migrate')
 
     def startapp(s):
@@ -75,19 +235,23 @@ class Main:
         s.log('Created app: %s' % app_name)
 
     def add_app_to_settings(s):
-        settings_file_path = s.get_full_path('/%s/%s/settings.py' % (s.proj_name, s.proj_name))
-        new_line = "\t'%s',\n" % s.app_name
-        s.insert_in_file(settings_file_path,
-                         'django.contrib.staticfiles',
-                         new_line)
-        s.log('Added "%s" to INSTALLED_APPS' % s.app_name)
+        #settings_file_path = s.get_full_path('/%s/%s/settings.py' % (s.proj_name, s.proj_name))
+        settings_file_path = '%s/settings.py' % (s.proj_name)
+        s.settings_file = settings_file = s.get_file(settings_file_path)
 
-    def insert_in_file(s, file_path, pattern, new_line):
-        lines = s.get_lines_from_file(file_path)
-        line_number = s.find_line_number(pattern, lines)
-        new_lines = s.add_in_list(new_line, line_number+1, lines, one_line=True)
-        #print(new_lines[:50])
-        s.write_to_file(new_lines, file_path)
+        new_line = "\t'%s'," % s.app_name
+        settings_file.insert_in_file('django.contrib.staticfiles', new_line)
+        #s.log('Added "%s" to INSTALLED_APPS' % s.app_name)
+
+    def add_db_to_settings(s):
+        s.db_type = db_type = SETTINGS.get('database')
+        if db_type not in ('mysql', 'postgres'):
+            return
+        db_settings_txt = TEXT['%s_db_settings' % db_type]
+        db_settings_txt = db_settings_txt % s.proj_name
+
+        s.settings_file.remove_lines("'default': {", '}')
+        s.settings_file.insert_in_file('DATABASES = {', db_settings_txt)
 
     def create_mng_cmd_dir(s, silent=True):
         s.app_dir = parent_dir = s.get_full_path('%s/%s' % (s.proj_name, s.app_name))
@@ -97,24 +261,33 @@ class Main:
 
     def create_demo_mng_cmd(s, silent=True):
         demo_mng_cmd_path = '%s/stub.py' % s.mng_cmd_dir
-        s.create_file(demo_mng_cmd_path, silent=True)
+        mng_cmd_file = s.create_file(demo_mng_cmd_path, silent=True)
         #TODO: from %s.models import
         mngcmd_txt = TEXT['mngcmd_txt']
 
-        s.write_to_file(mngcmd_txt, demo_mng_cmd_path)
+        mng_cmd_file.write_to_file(mngcmd_txt)#, demo_mng_cmd_path)
 
     def create_models(s):
         # TODO: Add BaseModel
-        model_path = s.get_full_path('%s/models.py' % (s.app_dir))
+        s.models_file = models_file = File('%s/models.py' % (s.app_dir))
         models_txt = TEXT['models']
+        s.models = models = TEXT['example_models']
+        models_txt = models_txt % models[0]
 
-        lines = s.get_lines_from_file(model_path)
-        pattern = 'from django.db import models'
-        line_number = s.find_line_number(pattern, lines)
+        models_file.insert_in_file('# Create your models here.', models_txt)
+        models_file.remove_lines('# Create your models here.')
 
-        new_lines = s.add_in_list(models_txt, line_number+1, lines)
-        #print(new_lines)
-        s.write_to_file(new_lines, model_path)
+    def add_models_to_admin(s):
+        s.admin_file = admin_file = File('%s/admin.py' % s.app_dir)
+        admin_txt = 'from %s.models import %s\n\n' % (s.app_name, ', '.join(s.models))
+        for model_name in s.models:
+            admin_txt += 'admin.site.register(%s)' % model_name
+        admin_file.insert_in_file('# Register your models here.', admin_txt)
+        admin_file.remove_lines('# Register your models here.')
+
+    def get_file(s, file_path):
+        f = File(file_path)
+        return f
 
     def create_module_dir(s, path, silent=False):
         s.create_dir(path, silent=silent)
@@ -125,39 +298,7 @@ class Main:
 
     def create_file(s, file_path, silent=False):
         s.call('touch %s' % file_path, silent=silent)
-
-    def write_to_file(s, content, file_path):
-        line_mode = False
-        if isinstance(content, list) or isinstance(content, tuple):
-            line_mode = True
-
-        file_path = s.get_full_path(file_path)
-
-        tmp_file_path = file_path + '.old'
-        if s.file_exists(file_path):
-            s.mv(file_path, tmp_file_path, silent=True)
-
-        with open(file_path, 'w') as f:
-            if line_mode:
-                for line in content:
-                    if isinstance(line, list):
-                        for l in line:
-                            f.write(l+'\n')
-                        continue
-                    #try:
-                    f.write(line)
-                    #except:
-                    #    print(line)
-            else:
-                f.write(content)
-
-        s.rm(tmp_file_path, silent=True)
-
-    def add_in_list(s, new_item, num, llist, one_line=False):
-        if isinstance(new_item, str) and not one_line:
-            new_item = new_item.split('\n')
-        llist = llist[:num] + [new_item] + llist[num:]
-        return llist
+        return File(file_path)
 
     def get_lines_from_file(s, filename):
         if not s.curr_dir in filename:
@@ -168,44 +309,18 @@ class Main:
             lines = f.readlines()
         return lines
 
-    def get_full_path(s, path):
-        if path[0] != '/':
-            path = '/' + path
-        if not s.auto_dir in path:
-            new_path = s.auto_dir + path
-            return new_path
-        return path
-
-    def find_line_number(s, pattern, lines):
-        for i, line in enumerate(lines):
-            if pattern in line:
-                return i
-        return None
-
     def cd(s, new_dir):
         if '/' not in new_dir:
             s.curr_dir = curr_dir = os.getcwd()
             os.chdir(curr_dir + '/%s' % new_dir)
         s.log('Changed directory to: %s' % os.getcwd(), tabs=1)
 
-    def mv(s, old_file_path, new_file_path, silent=False):
-        s.call('mv %s %s' % (old_file_path, new_file_path), silent=silent)
 
-    def rm(s, file_path, silent=False):
-        s.call('rm %s' % file_path, silent=silent)
-
-    def file_exists(s, file_path):
-        return os.path.exists(file_path)
-
-    def call(s, cmd, silent=False):
-        if isinstance(cmd, str):
-            cmd = [c for c in cmd.split(' ') if c != '']
-
-        if not s.args.get('test_run'):
-            run_shell_cmd(cmd, silent=silent)
-
-    def log(s, text, tabs=0):
-        print('\t'*tabs + text)
+def args_to_kwargs(args):
+    d = {}
+    for k,v in args._get_kwargs():
+        d[k] = v
+    return d
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -214,4 +329,5 @@ if __name__ == "__main__":
     parser.add_argument('--reset', action='store_true')
     args = parser.parse_args()
 
-    Main(**args_to_kwargs(args))
+    m = Main(**args_to_kwargs(args))
+    m.create_django_project()
--- config.py
+++ config.py
@@ -14,6 +14,7 @@ SETTINGS = {
 TEXT = {
     #
     'mngcmd_txt': """from django.core.management.base import BaseCommand, CommandError
+from app_demo.models import StubModel
 
 
 class Command(BaseCommand):
@@ -22,22 +23,43 @@ class Command(BaseCommand):
 
     def add_arguments(s, parser):
         pass
-        #parser.add_argument('--url', nargs='+', type=str)
+        # parser.add_argument('--url', nargs='+', type=str)
+        # parser.add_argument('--name', nargs='+', type=str)
 
     def process_args(s, options):
         pass
-        # return url
+        # url = options['url'] if options.get('url') else None
+        # name = options['name'] if options.get('name') else None
+        # return url, name
 
     def handle(s, *args, **options):
-        print('Hello World!')""",
+        # url, name = s.process_args(options)
+        print('Hello World!')
+        stub = StubModel(num_legs=2, good_bad=True)
+        stub.save()
+    """,
 
     #
-    'models': """class FauxModel(models.Model):
+    'models': """class %s(models.Model):
 
     num_legs = models.IntegerField(null=True)
-    good_bad = models.BooleanField()
+    is_valid = models.BooleanField()
 
     # def __str__(s):
     #     return 
+    """,
+
+    #
+    "example_models": ('StubModel',),
+
+    #
+    "postgres_db_settings": """    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '%s',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
     """
 }
